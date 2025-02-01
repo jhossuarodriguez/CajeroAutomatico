ee289ce6d26e0b672569361fde609644df91b260
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-02-01 18:34:29 -0400

    Actualización de UI y dependencias en Cajero Automático
    Se ha actualizado el archivo `CajeroAutomatico.csproj` para incluir referencias a los paquetes `bootstrap` versión `5.3.3` y `Microsoft.VisualStudio.LiveShare` versión `1.1.57`.
    
    En `Index.cshtml`:
    - Modificación de la clase del `<section>` a `container-hero`.
    - Cambio del título de la página a "Cajero Automatico".
    - Añadida imagen con clase `logo`.
    - Eliminados campos de calculadora de préstamos y ajustados campos del formulario.
    - Cambio del texto del botón a "Retirar".
    
    En `Privacy.cshtml`:
    - Eliminado contenido relacionado con la política de privacidad.
    
    En `_Layout.cshtml`:
    - Corregido el título de la página a "Cajero Automatico".
    - Cambio del texto del enlace de navegación a "Inicio".
    - Eliminado enlace a la política de privacidad en el pie de página.
    
    En `site.css`:
    - Ajustado el ancho del cuerpo a `100vw`.
    - Modificado estilo de `.container` y añadida clase `.container-hero`.
    - Añadidos márgenes a `label`.
    - Añadida clase `.logo` para posicionar la imagen del logo.
    - Ajustado estilo de `.form` para mayor responsividad.
    - Añadida clase `.boton` para estilizar el botón.
    
    Añadido nuevo controlador `CajeroController.cs` con acción `Index` que devuelve una vista.

---
+++ CajeroAutomatico/Controllers/CajeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CajeroAutomatico.Controllers
+{
+    public class CajeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
